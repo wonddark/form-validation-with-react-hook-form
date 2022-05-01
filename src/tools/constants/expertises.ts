53195e68d19a17242c59028dd4fb11389c9da46a
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2022-05-01 02:08:30 -0400

    A predefined list of expertises

---
+++ src/tools/constants/expertises.ts
@@ -0,0 +1,9 @@
+const expertises = [
+    {value: 'frontend', label: 'Frontend programming'},
+    {value: 'backend', label: 'Backend programming'},
+    {value: 'apk', label: 'Android programming'},
+    {value: 'ios', label: 'iOS programming'},
+    {value: 'Server', label: 'Server administration'},
+]
+
+export default expertises
